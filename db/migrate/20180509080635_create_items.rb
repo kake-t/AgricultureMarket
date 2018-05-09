006f8cf2b77fa8769cb0649b5925ea893412eefa
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-05-09 18:06:49 +0900

    fix Item table

--- db/migrate/20180509080635_create_items.rb
+++ db/migrate/20180509080635_create_items.rb
@@ -3,8 +3,12 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.string :item_name, null: false
       t.string :item_image, null: false
-      t.boolean :state, null: false, default: false
+      t.boolean :state, default: false
       t.integer :price, null: false
+      t.integer :saler_id
+      t.integer :buyer_id
+      t.text :item_content
+      t.references :producer, index: true
 
       t.timestamps
     end
