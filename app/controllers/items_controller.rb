8485163c276c7db5353cdb77b164f1fe6a2304c6
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-05-10 17:29:07 +0900

    add item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+  def top; end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,
+                                 :item_image,
+                                 :item_image_cache,
+                                 :price,
+                                 :item_content)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
-  
+  root 'items#top'
+
+  resources :items
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
   end
