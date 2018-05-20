10ba24ff59d6dbd34582af0412becfbe0043012f
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-05-21 04:02:06 +0900

    フォロー機能実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,13 @@
 class RelationshipsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    current_user.active_relationships.create!(followed_id: @user.id)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.active_relationships.find_by(followed_id: @user.id).destroy
+    redirect_to user_path(params[:user_id])
+  end
 end
