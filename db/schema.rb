232c5ea04f9663971e9e8c37b14409f04d6c3212
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-05-23 18:40:07 +0900

    Add Transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520093824) do
+ActiveRecord::Schema.define(version: 20180523093535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 20180520093824) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_transactions_on_item_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -87,4 +96,6 @@ ActiveRecord::Schema.define(version: 20180520093824) do
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "producers", "users"
+  add_foreign_key "transactions", "items"
+  add_foreign_key "transactions", "users"
 end
