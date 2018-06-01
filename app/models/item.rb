68303a26be3dbc158a685a1e4987d171e71cad96
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-06-02 01:15:23 +0900

    Add validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,9 @@ class Item < ApplicationRecord
 
   has_many :transactions, dependent: :destroy
 
-  validates :item_name, presence: true,
-                        length: {maximum: 150}
+  validates :item_name, presence: true, length: { maximum: 150 }
   validates :item_image, presence: true
+  validates :state, inclusion: { in: [true, false] }
+  validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
+  validates :item_content, presence: true, length: { maximum: 5000 }
 end
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -2,4 +2,9 @@ class Producer < ApplicationRecord
   mount_uploader :producer_image, ImageUploader
 
   belongs_to :user
+
+  validates :area, presence: true
+  validates :pesticide, inclusion: { in: [true, false] }
+  validates :producer_image, presence: true
+  validates :content, presence: true
 end
