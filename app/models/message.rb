66d51c4a2364698f7d4fd82c5f91e738b2270127
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-08-14 22:48:24 +0900

    Messageのマイグレーションファイルを編集

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :transaction
   belongs_to :user
+
+  validates_persence_of :body, :transaction_id, :user_id
+
+  def message_time
+    created_at.strftime("%m/%d/%y at %l:%M %p")
+  end
 end
--- db/migrate/20180814133939_create_messages.rb
+++ db/migrate/20180814133939_create_messages.rb
@@ -4,9 +4,9 @@ class CreateMessages < ActiveRecord::Migration[5.1]
       t.text :body
       t.references :transaction, foreign_key: true
       t.references :user, foreign_key: true
-      t.boolean :read
+      t.boolean :read, default: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
