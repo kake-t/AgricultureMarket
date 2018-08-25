6f64af1e95e29196c306e0e646006577e304b5ad
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-08-25 16:52:18 +0900

    Messageモデルからoptional: true を削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :negotiation, class_name: 'Transaction', optional: true
-  belongs_to :user, optional: true
+  belongs_to :negotiation, class_name: 'Transaction'
+  belongs_to :user
 
   validates_presence_of :body, :transaction_id, :user_id
 end
