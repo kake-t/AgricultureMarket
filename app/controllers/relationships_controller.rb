b33a23a22ae5d70fb640c84f83d468421cda3208
掛谷友博 <aaaaaaaaaaaaaaaaaaaaa>
2018-08-27 19:26:28 +0900

    close #38 Userモデルのfollow!メソッドの名前をfollowに変更 git commit -m Userモ

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,7 +8,7 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    current_user.follow!(@user)
+    current_user.follow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -17,7 +17,7 @@ class RelationshipsController < ApplicationController
 
   def destroy
     @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
+    current_user.unfollow(@user)
     respond_to do |format|
       format.html { redirect_to user_path(params[:user_id]) }
       format.js
