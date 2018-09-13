72127100f67d24fab2e1e010f6d25b3a7b3f163e
kakeyatomo <danbain.tobimasu@gmail.com>
2018-09-13 23:56:44 +0900

    Messageモデルからoptional:trueを削除

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :negotiation, class_name: 'Transaction'
+  belongs_to :negotiation, class_name: 'Transaction', optional: true
   belongs_to :user
 
   validates_presence_of :body, :transaction_id, :user_id
